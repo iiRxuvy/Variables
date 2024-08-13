516ef166c8079e7456e2f6e1583fab39ba5ddf90
Joseph Bosio <bosiojos001@warren.k12.in.us>
2024-08-13 10:16:49 -0400

    initial commmit

---
+++ main.py
@@ -0,0 +1,8 @@
+num_apples = 20
+num_oranges = 15
+print("Numbers of apples:" , num_apples)
+print("Numbers of oranges:" , num_oranges)
+num_oranges = 0
+print("Numbers of apples:" , num_apples)
+print("Numbers of oranges:" , num_oranges)
+
